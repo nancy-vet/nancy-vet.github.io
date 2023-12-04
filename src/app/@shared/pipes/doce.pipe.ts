0471d1a16b543f7ae1ce6b9d583f4500a4e6025a
Nancy Taneva <nancy.taneva@gmail.com>
2023-12-04 23:43:47 +0200

    Drugs new update

--- src/app/@shared/pipes/doce.pipe.ts
+++ src/app/@shared/pipes/doce.pipe.ts
@@ -18,6 +18,7 @@ export class DosePipe implements PipeTransform {
     if(value == 'ml_4.5_kg'         ) return 'ml/4.5 kg';
     if(value == 'ml_5_kg'           ) return 'ml/5 kg';
     if(value == 'ml_10_kg'          ) return 'ml/10 kg';
+    if(value == 'tabl'              ) return 'tabl';
     if(value == 'tabl_5_kg'         ) return 'tabl/5 kg';
     if(value == 'tabl_2.5_kg'       ) return 'tabl/2.5 kg';
     if(value == 'caps_5_kg'         ) return 'caps/5 kg';
