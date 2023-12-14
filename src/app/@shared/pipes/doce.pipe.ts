280bdda870e0470cbb31c014c290ed0c0db47f9b
Nancy Taneva <nancy.taneva@gmail.com>
2023-12-14 21:15:01 +0200

    Drugs new update, vetexpert

--- src/app/@shared/pipes/doce.pipe.ts
+++ src/app/@shared/pipes/doce.pipe.ts
@@ -12,6 +12,7 @@ export class DosePipe implements PipeTransform {
     if(value == 'µg_kg'             ) return 'µg/kg';
     if(value == 'g_kg'              ) return 'g/kg';
     if(value == 'g'                 ) return 'g';
+    if(value == 'mg'                ) return 'mg';
     if(value == 'ml'                ) return 'ml';
     if(value == 'ml_kg'             ) return 'ml/kg';
     if(value == 'ml_4_kg'           ) return 'ml/4 kg';
@@ -23,14 +24,17 @@ export class DosePipe implements PipeTransform {
     if(value == 'µg_tabl'           ) return 'µg/tabl';
     if(value == 'tabl_5_kg'         ) return 'tabl/5 kg';
     if(value == 'tabl_2.5_kg'       ) return 'tabl/2.5 kg';
-    if(value == 'caps_5_kg'         ) return 'caps/5 kg';
     if(value == 'tabl_10_kg'        ) return 'tabl/10 kg';
+    if(value == 'tabl_15_kg'        ) return 'tabl/15 kg';
+    if(value == 'tabl_40_kg'        ) return 'tabl/40 kg';
     if(value == 'implant'           ) return 'имплант';
     if(value == 'gtt'               ) return 'gtt';
     if(value == 'gtt_2_kg'          ) return 'gtt/2 kg';
     if(value == 'cm'                ) return 'cm';
     if(value == 'ml'                ) return 'ml';
     if(value == 'caps'              ) return 'caps';
+    if(value == 'caps_5_kg'         ) return 'caps/5 kg';
+    if(value == 'caps_10_kg'        ) return 'caps/10 kg';
     if(value == 'sprays'            ) return 'sprays';
     if(value == 'ampula'            ) return 'ампула';
     if(value == 'paketche'          ) return 'пакетче';
