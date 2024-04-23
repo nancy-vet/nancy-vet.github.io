35de991acee447fa54868cf88ac0cd3e50520c7a
Nancy Taneva <nancy.taneva@gmail.com>
2024-04-23 22:25:31 +0300

    Pregnancy cat before 40 days

--- src/app/@features/section-ultrasound/pregnancy/pregnancy.component.ts
+++ src/app/@features/section-ultrasound/pregnancy/pregnancy.component.ts
@@ -12,23 +12,32 @@ export class PregnancyComponent {
     when        : "before"
   };
 
-  public GSD: number = 0;
-  public CRL: number = 0;
-  public HD: number = 0;
-  public BD: number = 0;
+  public GSD  : number = 0;
+  public CRL  : number = 0;
+  public HD   : number = 0;
+  public BD   : number = 0;
 
-  public GAB: any;
-  public GAB1: any;
-  public GAB2: any;
+  public GAB  : any;
+  public GAB1 : any;
+  public GAB2 : any;
+  public GAB3 : any;
 
-  public GAA: any;
-  public GAA1: any;
-  public GAA2: any;
-  public GAA3: any;
+  public GAA  : any;
+  public GAA1 : any;
+  public GAA2 : any;
+  public GAA3 : any;
 
-  public DBP1: any;
-  public DBP2: any;
+  public DBP1 : any;
+  public DBP2 : any;
 
+  public IGCD : number = 0;
+  public EGCD : number = 0;
+  public EL   : number = 0;
+
+
+  public FAD: number = 0;
+  public FBD: number = 0;
+  public FSD: number = 0;
 
   public $ui = {
     fetusAge          : 0,
@@ -74,6 +83,31 @@ export class PregnancyComponent {
 
       this.$ui.fetusAge         = this.GAA;
       this.$ui.daysBeforeTermin = this.DBP2;
+    }
+
+    if(this.$formProperty.animalType == 'cat' && this.$formProperty.when == 'before') {
+
+      this.GAB1  = Math.round(((this.IGCD + 11.566) / 1.368) *100) / 100;
+      this.GAB2  = Math.round(((this.EGCD + 12.130) / 1.602) *100) / 100;
+      this.GAB3  = Math.round(((this.EL + 31.430) / 2.0087) *100) / 100;
+
+      this.GAB   = Math.round(((this.GAB1 + this.GAB2 + this.GAB3) /3) * 100) / 100;
+      this.DBP1  = Math.round((61 - this.GAB) * 100) / 100;
+
+      this.$ui.fetusAge         = this.GAB;
+      this.$ui.daysBeforeTermin = this.DBP1;
+    }
+
+    if(this.$formProperty.animalType == 'cat' && this.$formProperty.when == 'after') {
+
+      this.GAA1  = Math.round(((25 * this.HD) + 3) *100) /100;
+      this.GAA2  = Math.round(((11 * this.BD) + 21) *100) /100;
+
+      this.GAA   = Math.round(((this.GAA1 + this.GAA2) /2) * 100) / 100;
+      this.DBP1  = Math.round((61 - this.GAA) * 100) / 100;
+
+      this.$ui.fetusAge         = this.GAA;
+      this.$ui.daysBeforeTermin = this.DBP1;
     }
   }
 }
