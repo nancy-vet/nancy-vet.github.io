fc461c07243971e406503eb1e2e5f70d9e6c6edb
= <mail@mail.bg>
2024-04-23 21:23:34 +0300

    update calculator

--- src/app/@features/section-ultrasound/pregnancy/pregnancy.component.ts
+++ src/app/@features/section-ultrasound/pregnancy/pregnancy.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PregnancyComponent {
 
+  public $formProperty = {
+    animalType  : "dog",
+    when        : "before"
+  };
+
   public GSD: number = 0;
   public CRL: number = 0;
   public HD: number = 0;
@@ -24,22 +29,51 @@ export class PregnancyComponent {
   public DBP1: any;
   public DBP2: any;
 
+
+  public $ui = {
+    fetusAge          : 0,
+    daysBeforeTermin  : 0
+  }
+
+  /**
+   * @author Mihail Petrov
+   * @param $event
+   */
+  public selectAnimalType($event: any) {
+    this.$formProperty.animalType = ($event.detail.checked) ? 'cat' : 'dog';
+  }
+
+  /**
+   * @author Mihail Petrov
+   * @param $event
+   */
+  public selectWhenType($event: any) {
+    this.$formProperty.when = ($event.detail.checked) ? 'after' : 'before';
+  }
+
   public processCalculation() {
 
-    this.GAB1  = Math.round(((6 * this.GSD) + 20) *100) /100;
-    this.GAB2  = Math.round(((3 * this.CRL) + 27) *100) /100;
-    this.GAB  = Math.round(((this.GAB1 + this.GAB2) /2)*100)/100;
+    if(this.$formProperty.animalType == 'dog' && this.$formProperty.when == 'before') {
 
-    console.log(this.GAB1);
-    console.log(this.GAB2);
-    console.log(this.GAB);
+      this.GAB1  = Math.round(((6 * this.GSD) + 20) *100) / 100;
+      this.GAB2  = Math.round(((3 * this.CRL) + 27) *100) / 100;
+      this.GAB   = Math.round(((this.GAB1 + this.GAB2) /2) * 100) / 100;
+      this.DBP1  = Math.round((61 - this.GAB) * 100) / 100;
 
-    this.GAA1  = Math.round(((15 * this.HD) + 20) *100) /100;
-    this.GAA2  = Math.round(((7 * this.BD) + 29) *100) /100;
-    this.GAA3  = Math.round(((6 * this.HD) + (3 * this.BD) + 30) *100) /100;
-    this.GAA   = Math.round(((this.GAA1 + this.GAA2 + this.GAA3) /3) *100) /100;
+      this.$ui.fetusAge         = this.GAB;
+      this.$ui.daysBeforeTermin = this.DBP1;
+    }
 
-    this.DBP1  = Math.round((61 - this.GAB)*100)/100;
-    this.DBP2  = Math.round((61 - this.GAA)*100)/100;
+    if(this.$formProperty.animalType == 'dog' && this.$formProperty.when == 'after') {
+
+      this.GAA1  = Math.round(((15 * this.HD) + 20) *100) /100;
+      this.GAA2  = Math.round(((7 * this.BD) + 29) *100) /100;
+      this.GAA3  = Math.round(((6 * this.HD) + (3 * this.BD) + 30) *100) /100;
+      this.GAA   = Math.round(((this.GAA1 + this.GAA2 + this.GAA3) /3) *100) /100;
+      this.DBP2  = Math.round((61 - this.GAA)*100)/100;
+
+      this.$ui.fetusAge         = this.GAA;
+      this.$ui.daysBeforeTermin = this.DBP2;
+    }
   }
 }
