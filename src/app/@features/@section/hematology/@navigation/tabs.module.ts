4b0feeda5c086b19b808d19c7e65b4d63563148c
= <mail@mail.bg>
2024-07-06 08:05:40 +0300

    update

---
+++ src/app/@features/@section/hematology/@navigation/tabs.module.ts
@@ -0,0 +1,51 @@
+import { Component, NgModule    } from '@angular/core';
+import { CommonModule           } from '@angular/common';
+import { FormsModule            } from '@angular/forms';
+import { RouterModule, Routes   } from '@angular/router';
+import { IonicModule            } from '@ionic/angular';
+
+import { DrugCard               } from 'nv@components/@blocks/drug-card/drug-card.component';
+import { PrimaryMenu            } from 'nv@features/@menu/primary-menu/primary-menu.component';
+
+@Component({
+  selector    : 'calculator-navigation',
+  templateUrl : 'tabs.page.html'
+})
+export class TabNavigation {}
+
+const routes: Routes = [
+  {
+    path      : '',
+    component : TabNavigation,
+    children  : [
+      {
+        path          : 'blood_collection',
+        loadChildren  : () => import('../blood-collection/tubes/tubes.module').then(m => m.FeatureModule)
+      },
+      {
+        path          : 'term',
+        loadChildren  : () => import('../term/term/term.module').then(m => m.TermPageModule)
+      },
+      {
+        path: '',
+        redirectTo    : 'tab1',
+        pathMatch     : 'full'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  declarations                    : [
+    TabNavigation
+  ],
+  imports                         : [
+    RouterModule.forChild(routes) ,
+    IonicModule                   ,
+    CommonModule                  ,
+    FormsModule                   ,
+    DrugCard                      ,
+    PrimaryMenu
+  ]
+})
+export class TabsPageModule {}
